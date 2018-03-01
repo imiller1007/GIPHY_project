245230ed574a9e7c6f0957646a0a6a3d401d9f62
Ian Miller <imiller1007@yahoo.com>
2018-02-28 22:12:44 -0800

    final version

---
+++ assets/javascript/app.js
@@ -0,0 +1,145 @@
+//dont forget prevent.default
+// intitial variables----------------------------------------------------------------
+var charList = [];
+var button = $("button").attr("id");
+// functions--------------------------------------------------------------------------
+
+function populateButts() {
+    
+    $("#yourButtons").empty();
+    for(var i = 0; i<charList.length; i++){
+        $("#yourButtons").append("<button id='char' gifsearch='" + charList[i] + "' type='submit'>"+
+        charList[i] + "</button>");
+    
+    }
+}
+
+function displayGifs() {
+
+    var gifQuery = $(this).attr("gifsearch");
+    console.log(gifQuery);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    gifQuery + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        
+    $("#gifsHere").empty();
+        var results = response.data;
+  
+        for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+  
+        var rating = results[i].rating;
+  
+        var p = $("<p>").text("Rating: " + rating);
+  
+        var personImage = $("<img>");
+        personImage.attr("src", results[i].images.fixed_height.url);
+  
+        gifDiv.prepend(p);
+        gifDiv.prepend(personImage);
+  
+        $("#gifsHere").prepend(gifDiv);
+  
+        }
+      
+      
+    });
+
+  }
+
+
+// main processes-----------------------------------------------------------------------
+
+$("#userSub").on("click", function(event) {
+    event.preventDefault();
+    var char = $("input").val().trim();
+    charList.push(char);
+    populateButts();
+    $("input").val("");
+    console.log(charList);
+});
+
+$("button").on("click", function() {
+    
+    if($(this).attr("data-person")){
+
+    var person = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+          $("#gifsHere").empty();
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div class='item'>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(personImage);
+
+          $("#gifsHere").prepend(gifDiv);
+
+        }
+    });
+    
+}
+
+});
+
+
+$(document).on("click", "#char", displayGifs);
+    
+
+    
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
